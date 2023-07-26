90524d1f79b25a449f1f8db7471916293a4cc2ab
Reece Atkins <Reecepowered2@hotmail.com>
2023-07-26 16:17:45 -0700

    Added Dessert class

---
+++ eCommerceSite/Models/Dessert.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eCommerceSite.Models
+{
+    /// <summary>
+    /// Represents a single dessert
+    /// </summary>
+    public class Dessert
+    {
+        /// <summary>
+        /// The unique identifier for each game product
+        /// </summary>
+        [Key]
+        public int DessertId { get; set; }
+
+        /// <summary>
+        /// The name of the dessert
+        /// </summary>
+        [Required]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// The sales price of the dessert
+        /// </summary>
+        [Range(0, 1000)]
+        public double price { get; set; }
+
+    }
+}
