3669b2d1f008db417d45b7ca49e53ec902760aa0
Reece Atkins <Reecepowered2@hotmail.com>
2023-07-26 19:49:48 -0700

    Added DB Context class

---
+++ eCommerceSite/Data/DessertContext.cs
@@ -0,0 +1,15 @@
+﻿using eCommerceSite.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace eCommerceSite.Data
+{
+    public class DessertContext : DbContext
+    {
+        public DessertContext(DbContextOptions<DessertContext> options) : base(options) 
+        {
+        
+        }
+
+        public DbSet<Dessert> Desserts { get; set; }
+    }
+}
