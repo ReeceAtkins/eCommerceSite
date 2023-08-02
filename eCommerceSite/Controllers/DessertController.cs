f4a510ffe43e199c2ebbd078477ab3ebc6ec75ea
Reece Atkins <Reecepowered2@hotmail.com>
2023-08-01 21:58:51 -0700

    Create game fully funtional, added some styling

--- eCommerceSite/Controllers/DessertController.cs
+++ eCommerceSite/Controllers/DessertController.cs
@@ -1,14 +1,37 @@
-﻿using eCommerceSite.Models;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
 using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerceSite.Controllers
 {
     public class DessertsController : Controller
     {
-        [HttpGet]
+        private readonly DessertContext _context;
+
+        public DessertsController(DessertContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Dessert dessert)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Desserts.Add(dessert); // prepares
+                _context.SaveChanges(); // executes pending inserts
+
+                ViewData["Message"] = $"{dessert.Name} was added successfully";
+                
+                return View();
+            }
+
+            return View(dessert);
         }
     }
 }
